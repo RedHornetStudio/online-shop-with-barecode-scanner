fd1096115bf9560b444cbcf23f843b5a247862a5
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2021-09-25 14:50:52 +0300

    Changed api key modified:   src/apis/barcodeLookup.js

--- src/apis/barcodeLookup.js
+++ src/apis/barcodeLookup.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const key = 'fdum2els10plhqcm0w7gan2yx88hhd';
+const key = 'jsn33hmo03rt5xvuykr5ne985a8w8v';
 
 const barcodeLookup = axios.create({
   baseURL: 'https://api.barcodelookup.com/v3/products',
